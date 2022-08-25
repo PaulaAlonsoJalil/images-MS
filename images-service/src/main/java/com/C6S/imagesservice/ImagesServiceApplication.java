9bf23183cec97c13254f328bc5a5f1f453cabf97
PaulaAlonsoJalil <paualonso397@gmail.com>
2022-08-25 18:00:20 +0200

    upload project and cofigurations

---
+++ images-service/src/main/java/com/C6S/imagesservice/ImagesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.C6S.imagesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagesServiceApplication.class, args);
+	}
+
+}
---
+++ images-service/src/test/java/com/C6S/imagesservice/ImagesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.C6S.imagesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
